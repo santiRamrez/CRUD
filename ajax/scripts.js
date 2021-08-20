b828c637abb5c5605b24ce41e4e47984ce1b548d
Santiago Ramirez <santi_coding@outlook.com>
2021-08-19 21:48:21 -0400

    Mastering CRUD and API Requests

---
+++ ajax/scripts.js
@@ -0,0 +1,6 @@
+const d = document,
+  $table = d.querySelector(".crud-table"),
+  $form = d.querySelector(".form"),
+  $title = d.querySelector(".crud-title"),
+  $template = d.querySelector("#crudTemplate").textContent,
+  $fragment = d.createDocumentFragment();
